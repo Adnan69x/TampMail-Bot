207daddbf543e08a64598a54e8e1bbacb435174f
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 21:21:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-# Place your sensitive information here
-API_TOKEN = 'your_telegram_api_token'
+# config.py
+API_TOKEN = '123456:ABCDEFGHIJLLJOdMttZ5hEZ78'  # Securely store your API token here
+BASE_URL = "https://api.mail.tm"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+MAX_MESSAGE_LENGTH = 4000
