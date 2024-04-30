76349df72b7ef62bf6f510f494beda19496425c0
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-30 12:46:25 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-# config.py
-API_TOKEN = '123456:ABCDEFGHIJLLJOdMttZ5hEZ78'  # Securely store your API token here
-BASE_URL = "https://api.mail.tm"
-HEADERS = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
-MAX_MESSAGE_LENGTH = 4000
+import os
+
+class Config:
+    API_TOKEN = os.getenv("API_TOKEN", "your_default_token_here")
+    # Add more configuration variables as needed
